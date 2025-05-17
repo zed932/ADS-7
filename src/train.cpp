667c72d06a166624a1a683a543036b3e3c889113
Sergey Mescheryakov <mescheryakovsrg@gmail.com>
2025-05-17 14:36:44 +0300

    Add files via upload

--- src/train.cpp
+++ src/train.cpp
@@ -1,2 +1,53 @@
 // Copyright 2021 NNTU-CS
 #include "train.h"
+#include <cstdlib>
+#include <ctime>
+#include <iostream>
+
+Train::Train() {
+  countOp = 0;
+  first = nullptr;
+}
+
+void Train::addCar(bool light) {
+  Car* newCar = new Car{ light, nullptr, nullptr };
+
+  if (first == nullptr) {
+    newCar->next = newCar;
+    newCar->prev = newCar;
+    first = newCar;
+  } else {
+    Car* tail = first->prev;
+    newCar->next = first;
+    newCar->prev = tail;
+    tail->next = newCar;
+    first->prev = newCar;
+  }
+}
+
+int Train::getLength() {
+  countOp = 0;
+  Car* cursor = first;
+  int lengthEstimate = 1;
+
+  if (!cursor->light) {
+    cursor->light = true;
+  }
+
+  cursor = cursor->next;
+  countOp++;
+
+  while (!cursor->light) {
+    cursor = cursor->next;
+    countOp++;
+    lengthEstimate++;
+  }
+
+  cursor->light = false;
+
+  return lengthEstimate;
+}
+
+int Train::getOpCount() {
+  return countOp;
+}
